e8f34fd7ec732363fa92ee227ad53969e0f59ee6
GitJefersonHub <jefersonalves.ti@gmail.com>
2024-02-09 10:27:23 -0300

    09 02 24

---
+++ APPFotos/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color:'blue',fontSize:30}}>BEM VINDO</Text>
+      <Text style={{color:'red',fontSize:24,}}>APPFotos</Text>
+      <Text>Jeferson Alves</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
